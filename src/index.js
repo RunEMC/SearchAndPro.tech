11345d5260e97719772e8db590d5dbd3f4d643b5
RunEMC <runlin.li@gmail.com>
2018-02-03 18:01:33 -0500

    Added https request

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import AllUserWrapper from './components/AllUserWrapper.js';
 
-const title = 'My Minimal React Webpackdasdsf Babel Setup';
+const title = 'My Minimal React Webpack Babel Setup';
 
 ReactDOM.render(
   <div>
@@ -11,5 +11,28 @@ ReactDOM.render(
   </div>,
   document.getElementById('app')
 );
+
+//HTTP stdlib request
+const https = require('https');
+
+var twitterhandle = "Laggy";
+
+https.get("https://searchandprotech.lib.id/checkSuicidal@0.0.0/?twitterHandle=" + twitterhandle, (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log("Data from resp: " + data);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+//End HTTP stdlib request
 
 module.hot.accept();
