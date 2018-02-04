a786fc3ef87aa7a3d560e4bd79e4411f18edf4f0
RunEMC <runlin.li@gmail.com>
2018-02-04 01:10:53 -0500

    Added icon

--- webpack.config.js
+++ webpack.config.js
@@ -13,24 +13,24 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(jpe?g|png|gif)$/i,   //to support eg. background-image property 
+        test: /\.(jpe?g|png|gif)$/i,   //to support eg. background-image property
         loader: "file-loader",
         query: {
           name: '[name].[ext]',
           outputPath: 'images/'
-          //the images will be emmited to public/assets/images/ folder 
-          //the images will be put in the DOM <style> tag as eg. background: url(assets/images/image.png); 
+          //the images will be emmited to public/assets/images/ folder
+          //the images will be put in the DOM <style> tag as eg. background: url(assets/images/image.png);
         }
       },
       {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,    //to support @font-face rule 
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,    //to support @font-face rule
         loader: "url-loader",
         query: {
           limit: '10000',
           name: '[name].[ext]',
           outputPath: 'fonts/'
-          //the fonts will be emmited to public/assets/fonts/ folder 
-          //the fonts will be put in the DOM <style> tag as eg. @font-face{ src:url(assets/fonts/font.ttf); }  
+          //the fonts will be emmited to public/assets/fonts/ folder
+          //the fonts will be put in the DOM <style> tag as eg. @font-face{ src:url(assets/fonts/font.ttf); }
         }
       },
       {
@@ -58,6 +58,8 @@ module.exports = {
   devServer: {
     contentBase: './public',
     hot: true
+  },
+  node: {
+    fs: "empty"
   }
 };
-
